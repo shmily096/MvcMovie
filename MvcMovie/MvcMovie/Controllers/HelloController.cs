4b2a3a465865588b541236498ae9c8ceb70449ea
Sway Zou (邹士伟) <Sway.Zou@quantacn.com>
2019-06-14 09:01:23 +0800

    QMS

---
+++ MvcMovie/MvcMovie/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloController : Controller
+    {
+        //
+        // GET: /Hello/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Test()
+        {
+            return View();
+        }
+        public string Welcome(int id=1,string name="sa")
+        {
+            return "RequestID="+id+",name="+name;
+        }
+
+    }
+}
---
+++ MvcMovie/MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "修改此模板以快速启动你的 ASP.NET MVC 应用程序。";
+            ViewBag.Send = "Hello World!";
+            string[] Itmes = new string[] { "中国", "美国", "德国" };
+            ViewBag.itemsA = Itmes;// viewbag是一个新的dynamic类型keyword的封装器 //ViewData["Items"] = items;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "你的应用程序说明页。";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "你的联系方式页。";
+
+            return View();
+        }
+    }
+}
