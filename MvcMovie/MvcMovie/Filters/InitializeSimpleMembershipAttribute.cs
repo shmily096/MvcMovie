65498f05733f3ba0717645f5dde2cf466928bf79
Sway Zou (邹士伟) <Sway.Zou@quantacn.com>
2019-06-14 20:01:05 +0800

    Sway QMS update

--- MvcMovie/MvcMovie/Filters/InitializeSimpleMembershipAttribute.cs
+++ MvcMovie/MvcMovie/Filters/InitializeSimpleMembershipAttribute.cs
@@ -38,7 +38,7 @@ namespace MvcMovie.Filters
                         }
                     }
 
-                    WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
+                    WebSecurity.InitializeDatabaseConnection("MovieConnection", "UserProfile", "UserId", "UserName", autoCreateTables: true);
                 }
                 catch (Exception ex)
                 {
